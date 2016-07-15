e01a26179cce9482d67ea199a6573dba31c169f7
YKShtykov <yakov.shtykov@gmail.com>
2016-07-15 22:13:45 +0300

    Added task1

---
+++ TimerCUI/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using TimerLogic;
+
+namespace TimerCUI
+{
+  /// <summary>
+  /// Class describer, writes string in consolewhen evetn happend
+  /// </summary>
+  class FirstSubscriber
+  {
+    /// <summary>
+    /// constructor describes object of class on event
+    /// </summary>
+    /// <param name="timer"></param>
+    public FirstSubscriber(Timer timer)
+    {
+      timer.TimeOut += InvokatedMethod;
+    }
+
+    /// <summary>
+    /// invocated method
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="eventArgs"></param>
+    public void InvokatedMethod(object sender, TimeOutEventArgs eventArgs)
+    {
+      Console.WriteLine("Method of the first describer was invoked by timer event. Waiting time: {0}", eventArgs.Time);
+    }
+  }
+
+  /// <summary>
+  /// Class describer, writes string in consolewhen evetn happend
+  /// </summary>
+  class SecondSubscriber
+  {
+    /// <summary>
+    /// constructor describes object of class on event
+    /// </summary>
+    /// <param name="timer"></param>
+    public SecondSubscriber(Timer timer)
+    {
+      timer.TimeOut += InvokatedMethod;
+    }
+
+    /// <summary>
+    /// invocated method
+    /// </summary>
+    public void InvokatedMethod(object sender, TimeOutEventArgs eventArgs)
+    {
+      Console.WriteLine("Method of the second describer was invoked by timer event. Waiting time: {0}", eventArgs.Time);
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Timer timer = new Timer();
+      FirstSubscriber first = new FirstSubscriber(timer);
+      SecondSubscriber second = new SecondSubscriber(timer);
+
+      Console.WriteLine("enter waiting time in seconds");
+      TimeSpan waiting = TimeSpan.FromSeconds(int.Parse(Console.ReadLine()));
+      timer.TimerOn(waiting);
+      Console.ReadKey();
+    }
+  }
+}
---
+++ TimerLogic/Timer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+
+namespace TimerLogic
+{
+  /// <summary>
+  /// Custom EventArgs parameter, that contains waiting time
+  /// </summary>
+  public sealed class TimeOutEventArgs: EventArgs
+  {
+    private readonly TimeSpan time;
+    public TimeOutEventArgs(TimeSpan time)
+    {
+      this.time = time;
+    }
+    public TimeSpan Time { get { return time; } }
+  }
+
+  /// <summary>
+  /// Class timer, that invokates methods of objects-describers
+  /// </summary>
+  public sealed class Timer
+  {
+    /// <summary>
+    /// event contains list of invocated methods
+    /// </summary>
+    public event EventHandler<TimeOutEventArgs> TimeOut = delegate { };
+
+    /// <summary>
+    /// Method initialising event
+    /// </summary>
+    /// <param name="e"></param>
+    private void OnTimeOut (TimeOutEventArgs e)
+    {
+      TimeOut(this, e);
+    }
+
+    /// <summary>
+    /// Method that start timer.
+    /// </summary>
+    /// <param name="waiting"> input TimeSpan parameter, sets time of waiting</param>
+    public void TimerOn(TimeSpan waiting)
+    {
+      Thread.Sleep(waiting);
+      OnTimeOut(new TimeOutEventArgs(waiting));
+    }
+
+  }
+}
