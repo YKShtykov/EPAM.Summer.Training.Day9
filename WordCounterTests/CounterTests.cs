cb19ba3f7b8d27661490ff8da3941572bfaa608f
YKShtykov <yakov.shtykov@gmail.com>
2016-07-15 20:41:08 +0300

    Initial commit

---
+++ WordCounterTests/CounterTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using WordsMemoryzation;
+using System;
+
+namespace WordCounterTests
+{
+  /// <summary>
+  /// Test class that tests WordCounter
+  /// </summary>
+  [TestFixture]
+  public class CounterTests
+  {
+    public Dictionary<string, int> result = new Dictionary<string, int>();
+    public Dictionary<string, int> complexResult = new Dictionary<string, int>();
+    /// <summary>
+    /// Method -initializer initialise dictionaries 
+    /// </summary>
+    [TestFixtureSetUp]
+    public void Initializer()
+    {
+      result.Add("string", 2);
+      complexResult.Add("string", 2);
+      complexResult.Add("int", 3);
+      complexResult.Add("false", 2);
+      complexResult.Add("bool", 1);
+    }
+
+    /// <summary>
+    /// Simple case
+    /// </summary>
+    /// <param name="a"></param>
+    [TestCase("string string")]
+    public void CounterTest(string a)
+    {
+      Dictionary<string, int> actual = WordCounter.GetWordsCount(a);
+
+      CollectionAssert.AreEqual(result, actual);
+    }
+
+    /// <summary>
+    /// More complex case
+    /// </summary>
+    /// <param name="a"></param>
+    [TestCase("string string int int int false, false bool")]
+    public void ComplexCounterTest(string a)
+    {
+      Dictionary<string, int> actual = WordCounter.GetWordsCount(a);
+
+      CollectionAssert.AreEqual(complexResult, actual);
+    }
+
+    /// <summary>
+    /// Case of string equals null
+    /// </summary>
+    [Test]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void NullCounterTest()
+    {
+      string a = null;
+      Dictionary<string, int> actual = WordCounter.GetWordsCount(a);
+    }
+  }
+}
---
+++ WordsMemoryzation/WordCounter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordsMemoryzation
+{
+  /// <summary>
+  /// Class consists mehtod that counts equal words
+  /// </summary>
+  public static class WordCounter
+  {
+    /// <summary>
+    /// Method counts equal words and returns dictionary 
+    /// in wich key is word and value is count the word in string
+    /// </summary>
+    /// <param name="text">input string, words will be counted in</param>
+    /// <returns>dictionary(string, int)</returns>
+    public static Dictionary<string, int> GetWordsCount(string text)
+    {
+      Dictionary<string, int> result = new Dictionary<string, int>();
+      if (ReferenceEquals(text, null)) throw new ArgumentNullException();
+
+      char[] charArray = text.ToCharArray();
+      for (int i = 0; i < charArray.Length; i++)
+      {
+        if (!char.IsLetter(charArray[i]))
+        {
+          charArray[i] = ' ';
+        }
+      }
+
+      string[] textArray = new string(charArray).Split(new char[] { ' ' });
+      foreach (var item in textArray)
+      {
+        if (!(item == string.Empty))
+        {
+          if (!result.ContainsKey(item))
+          {
+            result.Add(item, 1);
+          }
+          else
+          {
+            result[item]++;
+          }
+        }
+      }
+
+      return result;
+    }
+  }
+}
