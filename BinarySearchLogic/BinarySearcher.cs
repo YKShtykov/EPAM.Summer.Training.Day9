ca02c64c66b2746a905c9469131d1d4daa673251
YKShtykov <yakov.shtykov@gmail.com>
2016-07-16 13:17:03 +0300

    Added binary search for all types

---
+++ BinarySearchLogic/BinarySearcher.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace BinarySearchLogic
+{
+  /// <summary>
+  /// Class does Binary search for array of any values.
+  /// </summary>
+  public static class BinarySearcher
+  {
+    /// <summary>
+    /// Binary search method with default comparision hash codes
+    /// </summary>
+    /// <param name="array"></param>
+    /// <param name="searchedElement"></param>
+    /// <returns>number of searched element in array or -1 if array has no the element</returns>
+    public static int BinarySearch(object[] array,
+                            object searchedElement)
+    {
+      return BinarySearch(array, searchedElement, DefaultComparision);
+    }
+    /// <summary>
+    /// overloaded method takes array of values and user comparision type of wich int Comparision<T>(T lhs, T rhs)
+    /// </summary>
+    /// <param name="array"></param>
+    /// <param name="searchedElement"></param>
+    /// <param name="comparer"></param>
+    /// <returns>number of searched element in array or -1 if array has no the element</returns>
+    public static int BinarySearch<T>(T[] array,
+                            T searchedElement,
+                            Comparison<T> comparer)
+    {
+      if (ReferenceEquals(array, null)) throw new ArgumentException();
+      if (ReferenceEquals(searchedElement, null)) return -1;
+      if (ReferenceEquals(comparer, null)) throw new ArgumentException();
+
+      Sort(array, comparer);
+      int bot = 0;
+      int mid = array.Length / 2;
+      int top = array.Length;
+
+      while (top != bot)
+      {
+        int compareResult = comparer( array[mid], searchedElement);
+        if (compareResult>0)
+        {
+          top = mid;
+          mid = top / 2;
+        }
+        if (compareResult < 0)
+        {
+          bot = mid;
+          mid = (bot + top) / 2;
+        }
+        if (compareResult == 0) return mid;
+      }
+
+      return -1;
+    }
+
+    /// <summary>
+    /// Default comparision method
+    /// </summary>
+    /// <param name="lhs"></param>
+    /// <param name="rhs"></param>
+    /// <returns></returns>
+    private static int DefaultComparision(object lhs, object rhs)
+    {
+      return lhs.GetHashCode().CompareTo(rhs.GetHashCode());
+    }
+
+    /// <summary>
+    /// Sort of incoming array
+    /// </summary>
+    /// <param name="array"></param>
+    /// <param name="comparer"></param>
+    private static void Sort<T>(T[] array, Comparison<T> comparer)
+    {
+      bool sorted = false;
+      while (!sorted)
+      {
+        sorted = true;
+        for (int i = 0; i < array.Length-1; i++)
+        {
+          if (comparer(array[i], array[i + 1]) == 1)
+          {
+            Swap(ref array[i], ref array[i + 1]);
+            sorted = false;
+          }
+        }
+      }
+    }
+
+    private static void Swap<T>(ref T lhs, ref T rhs)
+    {
+      T temp = lhs;
+      lhs = rhs;
+      rhs = temp;
+    }
+  }
+}
---
+++ BinarySearchLogicTests/SearchTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using BinarySearchLogic;
+using NUnit.Framework;
+
+namespace BinarySearchLogicTests
+{
+  /// <summary>
+  /// Testing class for BinarySearch logic 
+  /// </summary>
+  [TestFixture]
+  public class SearchTest
+  {
+    /// <summary>
+    /// Test Binary search in string array
+    /// </summary>
+    [Test]
+    public void StringTest()
+    {
+      string[] sArray = { "string", "bool", "int" };
+      int result = 2;
+      Comparison<string> stringComparer = (lhs,rhs) => string.Compare(lhs, rhs); 
+      int actual = BinarySearcher.BinarySearch(sArray, "string", stringComparer);
+
+      Assert.AreEqual(result, actual);
+    }
+
+    /// <summary>
+    /// tests binary search in int array
+    /// </summary>
+    [Test]
+    public void IntTest()
+    {
+      int[] iArray = { 111, 222, 13 };
+      int result = 0;
+      Comparison<int> intComparer = (lhs, rhs) => lhs.CompareTo(rhs);
+      int actual = BinarySearcher.BinarySearch(iArray, 13, intComparer);
+
+      Assert.AreEqual(result, actual);
+    }
+  }
+}
